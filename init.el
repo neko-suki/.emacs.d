bde87588a6a19d9a189bcce45f4961d32411e9fb
neko-suki <hogehogehoge1111@gmail.com>
2018-05-26 23:26:05 +0900

    initial commitw

---
+++ init.el
@@ -0,0 +1,22 @@
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
