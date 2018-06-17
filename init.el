ecd388e8f0bb9c000281340a6192625cbea27dca
neko-suki <hogehogehoge1111@gmail.com>
2018-06-17 23:10:13 +0900

    Add flymake

--- init.el
+++ init.el
@@ -20,3 +20,38 @@
 (setq show-paren-style 'mixed)
 (set-face-background 'show-paren-match-face "grey")
 (set-face-foreground 'show-paren-match-face "black")
+
+;; reference http://d.hatena.ne.jp/suztomo/20080905/1220633281
+;; reference https://qiita.com/awakia/items/5c97b02dcc3c7fd20279
+;;;flymake
+(require 'flymake)
+
+(defun flymake-cc-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+
+;;エラーメッセージをミニバッファで表示させる
+(global-set-key "\C-e" 'flymake-goto-next-error)
+(global-set-key "\M-e" 'flymake-goto-prev-error)
+
+
+(push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
+
+(add-hook 'c++-mode-hook
+          '(lambda ()
+             (flymake-mode t)))
+
+
+;; gotoした際にエラーメッセージをminibufferに表示する
+(defun display-error-message ()
+  (message (get-char-property (point) 'help-echo)))
+(defadvice flymake-goto-prev-error (after flymake-goto-prev-error-display-message)
+  (display-error-message))
+(defadvice flymake-goto-next-error (after flymake-goto-next-error-display-message)
+  (display-error-message))
+(ad-activate 'flymake-goto-prev-error 'flymake-goto-prev-error-display-message)
+(ad-activate 'flymake-goto-next-error 'flymake-goto-next-error-display-message)
